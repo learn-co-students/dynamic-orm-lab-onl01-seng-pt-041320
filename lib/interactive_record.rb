fb71b5edd75500307cea6fd04e4f602f13658caa
OmarMontemayor <omarmontemayor11@yahoo.com>
2020-06-21 21:03:27 -0500

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,64 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end
+
+    def self.column_names
+        DB[:conn].results_as_hash = true
+
+        sql = "PRAGMA table_info(#{table_name})"
+    
+        table_columns = DB[:conn].execute(sql)
+        column_names = []
+        table_columns.each do |col|
+          column_names << col["name"]
+        end
+        column_names.compact
+    end
+
+    def initialize(options={})
+        options.each do |property, value|
+            self.send("#{property}=", value)
+        end
+    end
+
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+        DB[:conn].execute(sql)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end
+
+    def table_name_for_insert
+        self.class.table_name
+    end
+
+    def values_for_insert
+        values = []
+        self.class.column_names.each do |col_name|
+            values << "'#{send(col_name)}'" unless send(col_name).nil?
+        end
+        values.join(", ")
+    end
+
+    def col_names_for_insert
+        self.class.column_names.delete_if { |col| col == "id"}.join(", ")
+    end
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+        DB[:conn].execute(sql, name)
+    end
+
+    def self.find_by(property)
+        column_name = property.keys[0].to_s
+        value_name = property.values[0]
+        sql = <<-SQL
+            SELECT *
+            FROM #{self.table_name}
+            WHERE #{column_name} = ?
+        SQL
+        DB[:conn].execute(sql, value_name);
+    end
 end
